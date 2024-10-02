1cb857ee34cd09d8b1e728bd45becc31a620b967
sofiaFdMoraes <sofiafirmino372gmail.com>
2024-10-02 09:55:13 -0300

    modificado

---
+++ lógica de programação2-dio/funcoes.js
@@ -0,0 +1,11 @@
+torrar ()
+
+function torrar (){
+    console.log("Torrando pão.")
+    injetarPao ()
+}
+
+function injetarPao (){
+    console.log("Preparando para ejetar pão.")
+    console.log("Finalizado.")
+}
---
+++ lógica de programação2-dio/function_main.js
@@ -0,0 +1,19 @@
+main ()
+
+function main (){
+    getData()
+    checkValues()
+    sendToDatabase()
+}
+
+function getData(){
+    console.log("Pegando dados do usuário...")
+}
+
+function checkValues(){
+    console.log("Validando dados...")
+}
+
+function sendToDatabase(){
+    console.log("Cadastrando dados.")
+}
